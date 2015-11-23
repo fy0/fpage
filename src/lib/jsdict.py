0981fe0220d49e9b147c1ea4f60b58653cca58d9
fy <fy0@qq.com>
2015-11-23 15:49:13 +0800

    更新了 jsdict

--- src/lib/jsdict.py
+++ src/lib/jsdict.py
@@ -2,8 +2,13 @@
 
 
 class JsDict(dict):
-    __getattr__ = dict.__getitem__
+    def __getitem__(self, item):
+        return self.get(item)
+
+    def __repr__(self):
+        return '<jsDict ' + dict.__repr__(self) + '>'
+
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
+    __getattr__ = __getitem__
 
-    def __repr__(self): return '<jsDict ' + dict.__repr__(self) + '>'
